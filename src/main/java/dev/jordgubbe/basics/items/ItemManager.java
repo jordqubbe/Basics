7de127c110ff767a04cf75d003ffc9080a58b986
killiank <killian.keays@outlook.com>
2022-02-10 13:39:41 -0800

    Added some more methods to the Item lib makes it easier for me

--- src/main/java/dev/jordgubbe/basics/items/ItemManager.java
+++ src/main/java/dev/jordgubbe/basics/items/ItemManager.java
@@ -6,7 +6,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -24,32 +27,24 @@ public class ItemManager {
         createTeleportBowItem();
     }
 
-
-
     public static void createTripleShotBowItem() {
         ArrayList<String> lore = new ArrayList<>();
         lore.add("yes");
         tripleShotBow = Item.createItem(Colorize.format("&6Triple Shot Bow"), Material.BOW, 1, lore);
-        Item.addEnchant(tripleShotBow, Enchantment.ARROW_INFINITE, 1);
+        Item.addEnchant(tripleShotBow, Enchantment.DURABILITY, 10);
+        Item.addAttributeModifier(tripleShotBow, Attribute.GENERIC_MOVEMENT_SPEED, "generic.Speed", 1, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
     }
 
     public static void createTeleportBowItem() {
-        teleportBow = new ItemStack(Material.BOW, 1);
-        ItemMeta meta = teleportBow.getItemMeta();
-        meta.setDisplayName(ChatColor.GOLD + "Teleport Bow");
         List<String> lore = new ArrayList<>();
         lore.add(Colorize.format("&6Item Ability: Ender Warp &e&lLEFT-CLICK"));
         lore.add(ChatColor.GRAY + "Use the item's ability to launch an Ender Pearl");
         lore.add(ChatColor.GRAY + "just like you're throwing one");
-        meta.setLore(lore);
-        teleportBow.setItemMeta(meta);
+        teleportBow = Item.createItem(Colorize.format("&6Teleport Bow"), Material.BOW, 1, lore);
 
         // Recipe
-
         ShapedRecipe teleportBowRecipe = new ShapedRecipe(NamespacedKey.minecraft("teleport_bow"), teleportBow);
-        teleportBowRecipe.shape(" ES",
-                "EDS",
-                " ES");
+        teleportBowRecipe.shape(" ES", "EDS", " ES");
         teleportBowRecipe.setIngredient('E', Material.ENDER_EYE);
         teleportBowRecipe.setIngredient('S', Material.STRING);
         teleportBowRecipe.setIngredient('D', Material.DIAMOND);
